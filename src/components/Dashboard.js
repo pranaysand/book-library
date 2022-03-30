726b9afb4bd8e353f7363dc69ba2c300b629242e
DEXTERS-HOME\livings <pranay.livingstone@gmail.com>
2022-03-30 15:09:50 -0500

    Created dummy components.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,14 @@
+//Create below two vars as seperate components and replace here.
+var BooksAvailable;
+var BooksRented;
 function Dashboard() {
-  return <div className="Dashboard">Dashboard Component</div>;
+  return (
+    <div className="Dashboard">
+      Dashboard Component
+      <BooksAvailable />
+      <BooksRented />
+    </div>
+  );
 }
 
 export default Dashboard;
